95f644062dbf01e47d3b37c2c56d5e6bbef23223
stauffenger <lucas.miranda96@hotmail.com>
2019-07-07 02:27:12 -0300

    Iniciando implementação do JWT

--- src/acesso_ao_banco.js
+++ src/acesso_ao_banco.js
@@ -19,7 +19,7 @@ function novoClient() {
 }
 
 function getProjetos(request, response) {
-    clientBancoDeDados = novoClient()
+    let clientBancoDeDados = novoClient()
     clientBancoDeDados.connect()
     .then(() => console.log("Conexão bem sucedida com o banco de dados!"))
     .then(() => clientBancoDeDados.query("SELECT login as usuario FROM usuarios"))
@@ -29,10 +29,12 @@ function getProjetos(request, response) {
 }
 
 function login(request, response) {
-    clientBancoDeDados = novoClient()
+    let clientBancoDeDados = novoClient()
+    let senha = request.body.senha
+    let login = request.body.usuario
     clientBancoDeDados.connect()
     .then(() => console.log("Conexão bem sucedida com o banco de dados!"))
-    .then(() => clientBancoDeDados.query("SELECT senha = crypt($1, senha) as senha FROM usuarios WHERE login = $2", ["chiclete", "lucaspascoal"]))
+    .then(() => clientBancoDeDados.query("SELECT senha = crypt($1, senha) as senha FROM usuarios WHERE login = $2", [senha, usuario]))
     .then(resultado => response.json(resultado.rows))
     .catch(erro => console.error("Erro ao tentar conectar com o banco de dados.", erro))
     .finally(() => clientBancoDeDados.end())
--- src/index.js
+++ src/index.js
@@ -1,22 +1,41 @@
 const clientBancoDeDados = require('./acesso_ao_banco')
+const jsonwebtoken = require('jsonwebtoken')
 const express = require('express')
 const app = express()
-const PORT = process.env.PORT || 5000
+const SENHA_JWT = process.env.SENHA_JWT
+const PORTA = process.env.PORT || 5000
 
 app.get('/', indexHandler)
 app.get('/projetos', projetosHandler)
-app.get('/login', loginHandler)
+app.post('/login', loginHandler)
 
-app.listen(PORT)
+app.listen(PORTA)
+
+function autorizacao(request, response) {
+    let token = request.headers['Token']
+    if (token !== undefined) {
+    jsonwebtoken.verify(token, SENHA_JWT)
+    .then(true)
+    .cath(erro => console.error("Falha na autenticação.", erro))
+    }
+    response.send("Acesso não autorizado. Você não deveria estar aqui!")
+    return false
+}
 
 function indexHandler(request, response) {
-    response.send("Bem-vindo a API do site UXP!")
+    if (autorizacao(request, response)) {
+        response.send("Bem-vindo a API do site UXP!")
+    }
 }
 
 function projetosHandler(request, response) {
-    clientBancoDeDados.getProjetos(request, response)
+    if (autorizacao(request, response)) {
+        clientBancoDeDados.getProjetos(request, response)
+    }
 }
 
 function loginHandler(request, response) {
-    clientBancoDeDados.login(request, response)
+    if (autorizacao(request, response)) {
+        clientBancoDeDados.login(request, response)
+    }
 }
