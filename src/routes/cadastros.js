0071d0936b2aa6b5f082377507c6d241f3df96d8
stauffenger <lucas.miranda96@hotmail.com>
2019-08-24 11:15:25 -0300

    Readicionando arquivos que foram excluidos na reestruturação, alocando-os na nova pasta

---
+++ src/routes/cadastros.js
@@ -0,0 +1,125 @@
+const DATABASE_POSTGRE = process.env.DATABASE_POSTGRE
+const HOST_POSTGRE = process.env.HOST_POSTGRE
+const PASSWORD_POSTGRE = process.env.PASSWORD_POSTGRE
+const PORT_POSTGRE = process.env.PORT_POSTGRE
+const USER_POSTGRE = process.env.USER_POSTGRE
+const SSL_POSTGRE = process.env.SSL_POSTGRE
+
+const logs = require('./logs')
+const {Client} = require('pg')
+
+function novoClient() {
+    clientBancoDeDados = new Client({
+        user: USER_POSTGRE,
+        password: PASSWORD_POSTGRE,
+        host: HOST_POSTGRE,
+        port: PORT_POSTGRE,
+        database: DATABASE_POSTGRE,
+        ssl: SSL_POSTGRE
+    })
+    return clientBancoDeDados
+}
+
+function login(request, response) {
+    let clientBancoDeDados = novoClient()
+    let senha = request.body.senha
+    let usuario = request.body.usuario
+    clientBancoDeDados.connect()
+    .then(() => console.log("Conexão bem sucedida com o banco de dados!"))
+    .then(() => clientBancoDeDados.query("SELECT (senha = crypt($1, senha) AND status) as autenticacao, status FROM usuarios WHERE usuario = $2", [senha, usuario]))
+    .then(resultado => {
+        logs.login(usuario)
+        if (resultado.rows[0] === undefined) {
+            response.json({ "autenticacao" : false, "status" : null })
+        } else {
+            response.json(resultado.rows[0])
+        }
+    })
+    .catch(erro => console.error("Erro ao tentar conectar com o banco de dados.", erro))
+    .finally(() => clientBancoDeDados.end())
+}
+
+function logout(request, response) {
+    let usuario = request.body.usuario
+    logs.logout(usuario)
+}
+
+function getCadastro(request, response) {
+    let clientBancoDeDados = novoClient()
+    let usuario = request.body.usuario
+    clientBancoDeDados.connect()
+    .then(() => console.log("Conexão bem sucedida com o banco de dados!"))
+    .then(() => clientBancoDeDados.query("SELECT usuario, status FROM usuarios WHERE usuario = $2", usuario))
+    .then(resultado => {
+        if (resultado.rows[0] === undefined) {
+            response.json({ "usuario" : false })
+        } else {
+            response.json(resultado.rows[0])
+        }
+    })
+    .catch(erro => console.error("Erro ao tentar conectar com o banco de dados.", erro))
+    .finally(() => clientBancoDeDados.end())
+}
+
+function inserirCadastro(request, response) {
+    let clientBancoDeDados = novoClient()
+    let senha = request.body.senha
+    let usuario = request.body.usuario
+    clientBancoDeDados.connect()
+    .then(() => console.log("Conexão bem sucedida com o banco de dados!"))
+    .then(async () => {
+        await clientBancoDeDados.query("INSERT INTO usuarios(usuario, senha) VALUES($1, crypt($2, gen_salt('bf')))", [usuario, senha])
+        .catch(erro =>console.error("Erro ao tentar cadastrar usuario no banco de dados.", erro))
+    })
+    .then(response.json({ "query" : true }))
+    .catch(erro => {
+        console.error("Erro ao tentar cadastrar usuario no banco de dados.", erro)
+        response.json({ "query" : false })
+    })
+    .finally(() => clientBancoDeDados.end())
+}
+
+function deletarCadastro(request, response) {
+    /*let clientBancoDeDados = novoClient()
+    let usuario = request.body.usuario
+    clientBancoDeDados.connect()
+    .then(() => console.log("Conexão bem sucedida com o banco de dados!"))
+    .then( async () => {
+        await clientBancoDeDados.query("DELETE FROM usuarios WHERE usuario = $1", id_usuario)
+        .catch(erro =>console.error("Erro ao tentar deletar cadastro no banco de dados.", erro))
+    })
+    .then(response.json({ "query" : true }))
+    .catch(erro => {
+        console.error("Erro ao tentar deletar cadastro no banco de dados.", erro)
+        response.json({ "query" : false })
+    })
+    .finally(() => clientBancoDeDados.end())*/
+    let usuario = request.body.usuario
+    console.log("Tentativa de deletar o cadastro $1 no banco de dados", usuario)
+    response.json({ "query" : false })
+}
+
+function editarCadastro(request, response) {
+    let clientBancoDeDados = novoClient()
+    let senha = request.body.senha
+    let usuario = request.body.usuario
+    clientBancoDeDados.connect()
+    .then(() => console.log("Conexão bem sucedida com o banco de dados!"))
+    .then(async () => {
+        await clientBancoDeDados.query("UPDATE usuarios SET senha = $1 WHERE usuario = $2", [senha, usuario])
+        .catch(erro =>console.error("Erro ao tentar editar cadastro no banco de dados.", erro))
+    })
+    .then(response.json({ "query" : true }))
+    .catch(erro => {
+        console.error("Erro ao tentar editar cadastro no banco de dados.", erro)
+        response.json({ "query" : false })
+    })
+    .finally(() => clientBancoDeDados.end())
+}
+
+module.exports.login = login
+module.exports.logout = logout
+module.exports.getCadastro = getCadastro
+module.exports.inserirCadastro = inserirCadastro
+module.exports.deletarCadastro = deletarCadastro
+module.exports.editarCadastro = editarCadastro
---
+++ src/routes/projetos.js
@@ -0,0 +1,95 @@
+const DATABASE_POSTGRE = process.env.DATABASE_POSTGRE
+const HOST_POSTGRE = process.env.HOST_POSTGRE
+const PASSWORD_POSTGRE = process.env.PASSWORD_POSTGRE
+const PORT_POSTGRE = process.env.PORT_POSTGRE
+const USER_POSTGRE = process.env.USER_POSTGRE
+const SSL_POSTGRE = process.env.SSL_POSTGRE
+
+const {Client} = require('pg')
+
+function novoClient() {
+    clientBancoDeDados = new Client({
+        user: USER_POSTGRE,
+        password: PASSWORD_POSTGRE,
+        host: HOST_POSTGRE,
+        port: PORT_POSTGRE,
+        database: DATABASE_POSTGRE,
+        ssl: SSL_POSTGRE
+    })
+    return clientBancoDeDados
+}
+
+function getProjetos(request, response) {
+    let clientBancoDeDados = novoClient()
+    clientBancoDeDados.connect()
+    .then(() => console.log("Conexão bem sucedida com o banco de dados!"))
+    .then(() => clientBancoDeDados.query("SELECT titulo, descricao, usuario as autor, edicao FROM projetos, usuarios WHERE id_autor = id_usuario ORDER BY data_criacao DESC"))
+    .then(resultados => response.json(resultados.rows))
+    .catch(erro => console.error("Erro ao tentar conectar com o banco de dados.", erro))
+    .finally(() => clientBancoDeDados.end())
+}
+
+function inserirProjeto(request, response) {
+    let clientBancoDeDados = novoClient()
+    let titulo = request.body.titulo
+    let descricao = request.body.descricao
+    let usuario = request.body.usuario
+    clientBancoDeDados.connect()
+    .then(() => console.log("Conexão bem sucedida com o banco de dados!"))
+    .then(async () => {
+        let subquery = "(SELECT '" + titulo + "', '" + descricao + "', id_usuario FROM usuarios WHERE usuario like '" + usuario +"')"
+        await clientBancoDeDados.query("INSERT INTO projetos(titulo, descricao, id_autor) " + subquery)
+        .catch(erro =>console.error("Erro ao tentar cadastrar projeto no banco de dados.", erro))
+    })
+    .then(response.json({ "query" : true }))
+    .catch(erro => {
+        console.error("Erro ao tentar cadastrar projeto no banco de dados.", erro)
+        response.json({ "query" : false })
+    })
+    .finally(() => clientBancoDeDados.end())
+}
+
+function deletarProjeto(request, response) {
+    let clientBancoDeDados = novoClient()
+    let titulo = request.body.titulo
+    let usuario = request.body.usuario
+    clientBancoDeDados.connect()
+    .then(() => console.log("Conexão bem sucedida com o banco de dados!"))
+    .then(async () => {
+        let subquery = "(SELECT id_usuario FROM usuarios WHERE usuario = '" + usuario + "')"
+        await clientBancoDeDados.query("DELETE FROM projetos WHERE titulo = $1 AND id_autor = " + subquery , [titulo])
+        .catch(erro =>console.error("Erro ao tentar deletar projeto no banco de dados.", erro))
+    })
+    .then(response.json({ "query" : true }))
+    .catch(erro => {
+        console.error("Erro ao tentar deletar projeto no banco de dados.", erro)
+        response.json({ "query" : false })
+    })
+    .finally(() => clientBancoDeDados.end())
+}
+
+function editarProjeto(request, response) {
+    let clientBancoDeDados = novoClient()
+    let titulo = request.body.titulo
+    let tituloAntigo = request.body.tituloAntigo
+    let descricao = request.body.descricao
+    let usuario = request.body.usuario
+    clientBancoDeDados.connect()
+    .then(() => console.log("Conexão bem sucedida com o banco de dados!"))
+    .then(async () => {
+        let subquery = "(SELECT id_usuario FROM usuarios WHERE usuario = '" + usuario + "')"
+        await clientBancoDeDados.query("UPDATE projetos SET titulo = $1, descricao = $2, edicao = true WHERE titulo = $3 AND id_autor = " + subquery, [titulo, descricao, tituloAntigo])
+        .catch(erro =>console.error("Erro ao tentar editar projeto no banco de dados.", erro))
+    })
+    .then(response.json({ "query" : true }))
+    .catch(erro => {
+        console.error("Erro ao tentar editar projeto no banco de dados.", erro)
+        response.json({ "query" : false })
+    })
+    .finally(() => clientBancoDeDados.end())
+}
+
+module.exports.getProjetos = getProjetos
+module.exports.inserirProjeto = inserirProjeto
+module.exports.deletarProjeto = deletarProjeto
+module.exports.editarProjeto = editarProjeto
